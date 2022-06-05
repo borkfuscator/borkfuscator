7ba8789fcfc4e0536aeac04a868a537ad3e7c4de
thatretrodev <thatretrodev@outlook.com>
2022-06-05 09:23:46 -0400

    0.0.9

--- packages/borkfuscator/index.js
+++ packages/borkfuscator/index.js
@@ -5,13 +5,14 @@ function borkfuscator(inputText, word) {
 	let borked = false;
 
 	const unborked = "~!@#$%^&*()_+`-={}[]|\\;:'\",./<>?".split("");
+	const quotechars = "nst".split("");
 
 	for (let i = 0; i < splitInput.length; i++) {
 		if (unborked.includes(splitInput[i])) {
 			borkBuffer += splitInput[i];
 			borked = false;
 		}
-		else if (i != 0 && ((splitInput[i].toLowerCase() == "s" && splitInput[i - 1] == "'") || (splitInput[i].toLowerCase() == "n" && splitInput[i - 1] == "'"))) {
+		else if (i != 0 && (quotechars.includes(splitInput[i].toLowerCase()) && splitInput[i - 1] == "'")) {
 			borkBuffer += splitInput[i];
 			borked = false;
 		}
